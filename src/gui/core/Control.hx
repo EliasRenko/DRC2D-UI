8ca4aca9d5905d17d6095cbd6815e74077417028
Fedorenko Elias <FedorenkoElias@yandex.ru>
2020-11-13 10:56:36 +0300

    Commit to master, build #6

---
+++ src/gui/core/Control.hx
@@ -0,0 +1,12 @@
+package gui.core;
+
+import drc.core.EventDispacher;
+import gui.events.ControlEvent;
+
+class Control extends EventDispacher<ControlEvent> {
+
+    public function new() {
+        
+        super();
+    }
+}
---
+++ src/gui/events/ControlEvent.hx
@@ -0,0 +1,10 @@
+package gui.events;
+
+import gui.core.Control;
+
+typedef ControlEvent = {
+
+	var timestamp:Float;
+
+	var control:Control;
+}
